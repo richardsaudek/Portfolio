ded97ebf4788d50ece39c95958e9b4116610f21a
richardsaudek <richard.saudek@gmail.com>
2023-05-01 15:23:24 -0400

    finished nav bar

---
+++ components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+// import AboutImg from '../public/assets/about.jpg';
+
+const About = () => {
+  return (
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+            About
+          </p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-600'>
+          As a software developer with a diverse background in digital marketing, the arts, and teaching, I bring a unique perspective to my work. My passion for human-centered design is informed by my experience as a teaching artist in the world of theater, where I learned the importance of empathy and collaboration in creating engaging experiences for diverse audiences. With expertise in React frameworks and a commitment to staying up-to-date with the latest technologies, I am adept at developing functional designs that are both innovative and user-friendly. As a communicator and team player, I value open dialogue and welcome feedback as an opportunity for growth.
+
+
+          </p>
+          <p className='py-2 text-gray-600'>
+          With my background in digital marketing, I understand the importance of creating designs that are not only functional but also user-friendly and engaging for diverse audiences. My experience in this field has taught me the importance of data-driven decision-making and the value of testing and iterating to optimize user experience. Drawing on my knowledge of React frameworks and other cutting-edge technologies, I am adept at developing digital solutions that align with business goals and user needs. I’m a team player who values open communication and feedback, recognizing that collaboration is key to achieving successful outcomes.
+          </p>
+          <Link href='/#projects'>
+            <p className='py-2 text-gray-600 underline cursor-pointer'>
+              Check out some of my latest projects.
+            </p>
+          </Link>
+        </div>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          {/* <Image src={AboutImg} className='rounded-xl' alt='/' /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Main.jsx
@@ -0,0 +1,71 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { MdOutlineEmojiPeople } from 'react-icons/md';
+import MainLogo from '../public/assets/richard.png'
+
+
+const Main = () => {
+  return (
+    <div id='home' className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>
+            LET&#39;S collaborate!
+          </p>
+
+
+          <h1 className='py-4 text-gray-700'>
+            
+            
+          <span role='img' aria-label='Lightning Bolt'></span> My name is <span className='text-[#7e71fd]'>Richard.</span>
+
+
+          </h1>
+
+
+
+          <h1 className='py-2 text-gray-700'>I'm a Software Developer.</h1>
+          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
+            I work with creative people to make responsive web applications.
+
+          </p>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <a
+              href='https://www.linkedin.com/in/richard-saudek/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href='https://github.com/richardsaudek'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
+            <Link href='/#contact'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href='/resume'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsFillPersonLinesFill />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
